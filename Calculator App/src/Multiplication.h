18653db2ef6d29b019ba276b5122e72d9928355e
RIO GOLDING-GOODEN <RIO.GOLDING-GOODEN22@bathspa.ac.uk>
2024-03-05 15:53:46 +0000

    Multiply

---
+++ Calculator App/src/Multiplication.h
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+
+
+class Multiplication{
+
+	int multipy(int a, int b) {
+		return a * b;
+
+	}
+};
+
+int main() {
+	Multiplication multiplication;
+	int num1, num2;
+	std::cout << "Enter the first number: ";
+	cin >> num1;
+	cout << "Enter the second number: ";
+	cin >> num2;
+}
+
+int result = multiplication.multiply(num1, num2);
+cout << "Result: " << result << std::endl;
+
+return 0;
+}
