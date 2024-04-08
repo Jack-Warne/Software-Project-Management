22bafa83993fe27b830b513ae380d33b6d327a25
Elvin321 <115479881+Elvin321@users.noreply.github.com>
2024-04-08 17:33:33 +0100

    ui implementation numbers

--- Calculator App/src/main.cpp
+++ Calculator App/src/main.cpp
@@ -7,7 +7,7 @@ int main() {
 	//	A variation of the OpenFrameworks code for starting a window. This one also prevents the window from being resized
 	ofGLFWWindowSettings settings;
 	settings.resizable = false;
-	settings.setSize(1024, 768);
+	settings.setSize(400, 600);
 	ofCreateWindow(settings);
 
 	return ofRunApp(new ofApp);
